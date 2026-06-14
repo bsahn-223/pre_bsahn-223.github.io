5eb6342cd3f1b77fcbe9067b3fb49465700c1562
bsahn-223 <84bsahn@gmail.com>
2026-06-14 15:14:24 +0900

    Update GallerySection.tsx

--- src/components/section/GallerySection.tsx
+++ src/components/section/GallerySection.tsx
@@ -22,7 +22,7 @@ import useIsInView from "@/hooks/useIsInView";
 const getGalleryImageLoader = (number: number) => {
   return `/gallery/gallery_${number < 10 ? `0${number}` : number}.JPEG`;
 };
-const IMAGES = Array.from({ length: 11 }, (_, i) => ({
+const IMAGES = Array.from({ length: 18 }, (_, i) => ({
   url: getGalleryImageLoader(i + 1)
 }));
 
